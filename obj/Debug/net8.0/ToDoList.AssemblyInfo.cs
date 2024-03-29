45af1123c45d99e1a04889e8ec96a8788b94ced1
Mattéo Grellier <matteogrellier@gmail.com>
2024-03-29 13:06:27 +0100

    wip todo list test app

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4be5c6fd0891c28ec0e2fdf5b9093585facb8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
