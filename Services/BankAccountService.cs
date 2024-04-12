7d50c10cbb18ceafbb1f9e9c74d2798d8a70c7ed
Matteo Grellier <matteogrellier@gmail.com>
2024-04-12 12:40:41 +0200

    Create all services (#5)
    * feat(Services): Create all services
    
    * Fix(services): Refacto services

---
+++ Services/BankAccountService.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bankable.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Bankable.Services;
+
+public class BankAccountService
+{
+	BankableContext bankableContext = new();
+
+	public async Task<IEnumerable<BankAccount>> GetAllItems()
+	{
+		IEnumerable<BankAccount> bankAccounts;
+		bankAccounts = await bankableContext.BankAccounts.ToListAsync();
+		return bankAccounts;
+	}
+
+	public async Task<BankAccount> GetItemByID(Guid id)
+	{
+		var bankAccount = await bankableContext.BankAccounts.SingleAsync(e => e.Id == id);
+		return bankAccount;
+	}
+	public async Task<EntityEntry<BankAccount>> AddItem(BankAccount bankAccount)
+	{
+		var addedBankAccount = bankableContext.Add(bankAccount);
+		await bankableContext.SaveChangesAsync();
+		return addedBankAccount;
+	}
+
+	public async Task<EntityEntry<BankAccount>> UpdateItem(BankAccount bankAccount)
+	{
+		var updatedBankAccount = bankableContext.Update(bankAccount);
+		await bankableContext.SaveChangesAsync();
+		return updatedBankAccount;
+	}
+
+	public async Task<string> RemoveItem(BankAccount bankAccount)
+	{
+		bankableContext.Remove(bankAccount);
+		await bankableContext.SaveChangesAsync();
+		return "Item has been removed";
+	}
+
+
+}
