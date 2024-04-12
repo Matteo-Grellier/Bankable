5a1b35b6f9ec91d86bb707106e40708c10599d6d
Olivia Moreau <moreauolivia49@gmail.com>
2024-04-12 14:05:27 +0200

    feat: spending list WIP

--- ViewModels/BankAccounts/BankAccountsListViewModel.cs
+++ ViewModels/BankAccounts/BankAccountsListViewModel.cs
@@ -1,6 +1,21 @@
-namespace Bankable.ViewModels.BankAccounts;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Bankable.Models;
+using Bankable.ViewModels;
 
-public class BankAccountsListViewModel: ViewModelBase
+namespace AvaloniaControls.ViewModels
 {
-    
+public class BankAccountsListViewModel : ViewModelBase
+    {
+        public ObservableCollection<Spending> People { get; }
+
+    public BankAccountsListViewModel()
+        {
+            var spending = new List<Spending>
+            {
+                new Spending(),
+            };
+            People = new ObservableCollection<Spending>(spending);
+        }
+    }
 }
