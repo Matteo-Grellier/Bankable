91188362429ae6ceb1c3f7e5cae59bf9db480649
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2024-04-20 18:43:25 +0200

    feat(fixing debug usage of the gauge chart)

--- ViewModels/GaugeChartViewModel.cs
+++ ViewModels/GaugeChartViewModel.cs
@@ -18,32 +18,12 @@ public class GaugeChartViewModel: ViewModelBase
 {
     public GaugeChartViewModel()
     {
-        VTFF();
-    }
-    private async void VTFF()
-    {
-        IEnumerable<BankAccount> actualBankAccount = await _banAccountService.GetAllItems();
-        GetDataFromServices(actualBankAccount.First().Id, 1700);
+        DebugGetDataFromServices();
     }
 
     private BankAccountService _banAccountService = new BankAccountService();
 
-
-    public IEnumerable<ISeries> Series { get; set; } = GaugeGenerator.BuildSolidGauge(
-        new GaugeItem(30, series => {
-            series.Fill = new SolidColorPaint(SKColors.Green);
-            series.DataLabelsSize = 50;
-            series.DataLabelsPaint = new SolidColorPaint(SKColors.Black);
-            series.DataLabelsPosition = PolarLabelsPosition.ChartCenter;
-            series.InnerRadius = 100;
-            series.DataLabelsFormatter = point => $"{point.Coordinate.PrimaryValue} %";
-            series.ToolTipLabelFormatter = point => $"{point.StackedValue!.Share}";
-        }),
-        new GaugeItem( GaugeItem.Background, series => {
-            series.InnerRadius = 100;
-            series.Fill = new SolidColorPaint(new SKColor(100, 181, 246, 90));
-        })
-    );
+    public IEnumerable<ISeries> Series { get; set; }
 
 
     public async void GetDataFromServices(Guid bankAccountID, float moneyForTheMonth)
@@ -52,9 +32,16 @@ public class GaugeChartViewModel: ViewModelBase
         Series = ConvertToGaugeSeries(actualBankAccount.Amount, moneyForTheMonth);
     }
 
+
+    private async void DebugGetDataFromServices()
+    {
+        IEnumerable<BankAccount> actualBankAccount = await _banAccountService.GetAllItems();
+        GetDataFromServices(actualBankAccount.First().Id, 200000);
+    }
+
     private IEnumerable<ISeries> ConvertToGaugeSeries(float moneyAmount, float moneyForTheMonth)
     {
-        float percentage = moneyAmount / moneyForTheMonth * 100;
+        float percentage = (int)(moneyAmount / moneyForTheMonth * 100);
         SKColor gaugeColor;
 
         if (percentage > 50)
@@ -72,7 +59,7 @@ public class GaugeChartViewModel: ViewModelBase
                 series.DataLabelsPosition = PolarLabelsPosition.ChartCenter;
                 series.InnerRadius = 100;
                 series.DataLabelsFormatter = point => $"{point.Coordinate.PrimaryValue} %";
-                series.ToolTipLabelFormatter = point => $"{moneyAmount}€ - {point.Coordinate.PrimaryValue} % of {moneyForTheMonth}";
+                series.ToolTipLabelFormatter = point => $"{moneyAmount}€ ({point.Coordinate.PrimaryValue} % of {moneyForTheMonth})";
             }),
             new GaugeItem( GaugeItem.Background, series => {
                 series.InnerRadius = 100;
