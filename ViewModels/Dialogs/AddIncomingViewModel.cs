8521b569aa81537274cb137d95ceb87b19e7aa95
matheoleger <matheo.leger@ynov.com>
2024-04-21 00:39:06 +0200

    feat(add): Add incomes dialog

--- ViewModels/Dialogs/AddIncomingViewModel.cs
+++ ViewModels/Dialogs/AddIncomingViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using Bankable.Models;
 using Bankable.Services;
 using DialogHostAvalonia;
@@ -18,13 +19,55 @@ public class AddIncomingViewModel: ViewModelBase
     private float _amount;
     private List<BankAccount> _avalaibleBankAccounts;
     private List<Category> _availableCategories;
+
+    private Category _selectedCategory;
+    private BankAccount _selectedBankAccount;
     
     private CategoryService _categoryService = new();
     private BankAccountService _bankAccountService = new();
+    private IncomingService _incomingService = new();
     
     public ReactiveCommand<Unit, Incoming> ConfirmationCommand { get; }
+    
+    public float Amount
+    {
+        get => _amount;
+        set => this.RaiseAndSetIfChanged(ref _amount, value);
+    }
+    public string Title
+    {
+        get => _title;
+        set => this.RaiseAndSetIfChanged(ref _title, value);
+    }
+    public string Description
+    {
+        get => _description; 
+        set => this.RaiseAndSetIfChanged(ref _description, value);
+    }
 
+    public List<Category> AvailableCategories
+    {
+        get => _availableCategories; 
+        set => this.RaiseAndSetIfChanged(ref _availableCategories, value);
+    }
 
+    public List<BankAccount> AvailableBankAccounts
+    {
+        get => _avalaibleBankAccounts; 
+        set => this.RaiseAndSetIfChanged(ref _avalaibleBankAccounts, value);
+    }
+    
+    public Category SelectedCategory    
+    {
+        get => _selectedCategory; 
+        set => this.RaiseAndSetIfChanged(ref _selectedCategory, value);
+    }
+    public BankAccount SelectedBankAccount     
+    {
+        get => _selectedBankAccount; 
+        set => this.RaiseAndSetIfChanged(ref _selectedBankAccount, value);
+    }
+    
     public AddIncomingViewModel()
     {
         // Get categories and bank accounts when we instantiate the AddIncomingViewModel
@@ -40,51 +83,35 @@ public class AddIncomingViewModel: ViewModelBase
         ConfirmationCommand = ReactiveCommand.Create(
             () =>
             {
-                DialogHost.Close("AddDialog");
-                
-                return new Incoming { Description = Description, Title = Title };
+                try
+                {
+                    var newIncome = _incomingService.AddItem(
+                        new Incoming
+                        {
+                            Description = Description, 
+                            Title = Title, 
+                            Amount = Amount,
+                            BankAccountId = SelectedBankAccount.Id, 
+                            CategoryId = SelectedCategory.Id,
+                            Date = new DateTime()
+                        }
+                    ).Result.Entity;
+                    DialogHost.Close("AddDialog");
+                    return newIncome;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
             }, isValidObservable);
         // CancelCommand = ReactiveCommand.Create(() => { })
     }
     
-    public float Amount
-    {
-        get => _amount;
-        private set => this.RaiseAndSetIfChanged(ref _amount, value);
-    }
-    public string Title
-    {
-        get => _title;
-        private set => this.RaiseAndSetIfChanged(ref _title, value);
-    }
-    public string Description
-    {
-        get => _description; 
-        private set => this.RaiseAndSetIfChanged(ref _description, value);
-    }
-
-    public List<Category> AvailableCategories
-    {
-        get => _availableCategories; 
-        set => this.RaiseAndSetIfChanged(ref _availableCategories, value);
-    }
-
-    public List<BankAccount> AvailableBankAccounts
-    {
-        get => _avalaibleBankAccounts; 
-        set => this.RaiseAndSetIfChanged(ref _avalaibleBankAccounts, value);
-    }
-    
-    public Category SelectedCategory { get; set; }
-    public BankAccount SelectedBankAccount { get; set; }
-    
     private async void SetAvailableProperties()
     {
-        // await _categoryService.AddItem(new Category { Name = "Loisirs" });
-        // var availableCategories = await _categoryService.GetAllItems();
         AvailableCategories = await _categoryService.GetAllItems();
-
-        // var availableBankAccounts = await _bankAccountService.GetItemsByUser();
         AvailableBankAccounts = await _bankAccountService.GetItemsByUser();
     }
 }
