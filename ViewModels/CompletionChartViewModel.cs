0de59a0d2639b72bef6fc44b3b658191c1b87747
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2024-04-25 01:37:46 +0200

    feat(added completion chart working with services)

---
+++ ViewModels/CompletionChartViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Bankable.ViewModels.BankAccounts;
+using ReactiveUI;
+
+namespace Bankable.ViewModels;
+
+public class BankAccountsViewModel: ViewModelBase
+{
+    private ViewModelBase _contentViewModel;
+
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public void Dashboard()
+    {
+        ContentViewModel = new BankAccountsDashboardViewModel();
+    }
+
+    public void List()
+    {
+        ContentViewModel = new BankAccountsListViewModel();
+    }
+}
