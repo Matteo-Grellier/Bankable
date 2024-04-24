af84f7b3d0fe8cf25d9ea1af51834909d914b47c
Mathéo LEGER <71370253+matheoleger@users.noreply.github.com>
2024-04-24 23:32:01 +0200

    feat(DI): Add Dependency injection (#10)
    * feat(DI): Add Dependency injection
    
    * chore(DI): Remove useless code

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Bankable.ViewModels;
 using Bankable.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bankable;
 
@@ -15,11 +17,21 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        // Register all the services needed for the application to run
+        var collection = new ServiceCollection();
+        collection.AddViewModels();
+        collection.AddViews();
+        collection.AddServices();
+
+        // Creates a ServiceProvider containing services from the provided IServiceCollection
+        var services = collection.BuildServiceProvider();
+
+        var vm = services.GetRequiredService<MainWindowViewModel>();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = vm
             };
         }
 
