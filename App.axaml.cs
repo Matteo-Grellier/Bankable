da7609e96ab364f22fae4e18dbe6884712c0ab2a
matheoleger <matheo.leger@ynov.com>
2024-04-24 20:00:06 +0200

    chore(DI): Remove useless code

--- App.axaml.cs
+++ App.axaml.cs
@@ -17,10 +17,6 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // If you use CommunityToolkit, line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
-        BindingPlugins.DataValidators.RemoveAt(0);
-
         // Register all the services needed for the application to run
         var collection = new ServiceCollection();
         collection.AddViewModels();
