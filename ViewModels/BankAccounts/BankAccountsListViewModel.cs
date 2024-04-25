40a617ba0cf259ddb9d44ed4106e908654465ce9
Olivia MOREAU <73989303+Liv44@users.noreply.github.com>
2024-04-25 11:58:01 +0200

    feat(lists): add spendings and incomings lists (#15)
    * feat(lists): add spendings and incomings lists
    
    * feat(lists): change after review

--- ViewModels/BankAccounts/BankAccountsListViewModel.cs
+++ ViewModels/BankAccounts/BankAccountsListViewModel.cs
@@ -1,6 +1,33 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform;
+
 namespace Bankable.ViewModels.BankAccounts;
+using ReactiveUI;
 
 public class BankAccountsListViewModel: ViewModelBase
 {
-    
+    private ViewModelBase _contentViewModel;
+
+    public BankAccountsListViewModel()
+    {
+        ContentViewModel = new ListSpendingsViewModel();
+    }
+
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public void Spendings()
+    {
+        ContentViewModel = new ListSpendingsViewModel();
+    }
+
+    public void Incomes()
+    {
+        ContentViewModel = new ListIncomesViewModel();
+    }
 }
--- Views/BankAccounts/ListSpendingsView.axaml.cs
+++ Views/BankAccounts/ListSpendingsView.axaml.cs
@@ -5,9 +5,9 @@ using Avalonia.Markup.Xaml;
 
 namespace Bankable.Views.BankAccounts;
 
-public partial class BankAccountsListView : UserControl
+public partial class ListSpendingsView : UserControl
 {
-    public BankAccountsListView()
+    public ListSpendingsView()
     {
         InitializeComponent();
     }
