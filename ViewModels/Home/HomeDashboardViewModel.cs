c56288d067cc87c126293bde2fd5ef6c89db8bec
Olivia Moreau <moreauolivia49@gmail.com>
2024-04-26 05:35:16 +0200

    feat(dashboards): fix set values

--- ViewModels/Home/HomeDashboardViewModel.cs
+++ ViewModels/Home/HomeDashboardViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Bankable.Models;
@@ -15,37 +16,31 @@ public class HomeDashboardViewModel:ViewModelBase, IDashboardListViewModel
     private readonly IncomingService _incomingService = new();
     private readonly SavingProjectService _savingProjectService = new();
     private readonly SavingService _savingService = new();
-    
+
     private DateTimeOffset _selectedDate;
-    private List<Spending> _spendings;
-    private List<Incoming> _incomes;
-    private List<SavingProject> _savingProjects;
-    private List<Saving> _savings;
 
     public HomeDashboardViewModel()
     {
         _selectedDate = new DateTimeOffset(DateTime.Now);
         SetValues(_selectedDate);
-        SetLast3spendingsAsync();
-        SetReccurentAsync();
     }
-    
+
     private ViewModelBase _donutChartViewModel = new DonutChartViewModel();
     public ViewModelBase DonutChartViewModel
     {
         get => _donutChartViewModel;
         private set => this.RaiseAndSetIfChanged(ref _donutChartViewModel, value);
     }
-    
+
     private ViewModelBase _barsChartViewModel = new BarsChartViewModel();
     public ViewModelBase BarsChartViewModel
     {
         get => _barsChartViewModel;
         private set => this.RaiseAndSetIfChanged(ref _barsChartViewModel, value);
     }
-    
-    public DateTimeOffset SelectedDate 
-    { 
+
+    public DateTimeOffset SelectedDate
+    {
         get => _selectedDate;
         set
         {
@@ -53,103 +48,47 @@ public class HomeDashboardViewModel:ViewModelBase, IDashboardListViewModel
             this.RaiseAndSetIfChanged(ref _selectedDate, value);
         }
     }
-    
-    public List<Spending> Spendings 
+
+    private string _totalUsefull;
+    public string TotalUsefull
     {
-        get => _spendings; 
-        set => this.RaiseAndSetIfChanged(ref _spendings, value);
-    }
-    
-    public List<Incoming> Incomes 
-    {
-        get => _incomes; 
-        set => this.RaiseAndSetIfChanged(ref _incomes, value);
-    }
-    
-    public List<SavingProject> SavingProjects 
-    {
-        get => _savingProjects; 
-        set => this.RaiseAndSetIfChanged(ref _savingProjects, value);
+        get => _totalUsefull;
+        set => this.RaiseAndSetIfChanged(ref _totalUsefull, value);
     }
 
-    public List<Saving> Savings
+    private string _totalUseless;
+    public string TotalUseless
     {
-        get => _savings; 
-        set => this.RaiseAndSetIfChanged(ref _savings, value);
+        get => _totalUseless;
+        set => this.RaiseAndSetIfChanged(ref _totalUseless, value);
     }
-    
-    public string TotalUsefull 
+
+    private string _savedThisMonth;
+    public string SavedThisMonth
     {
-        get
-        {
-            List<Spending> allSpendings = _spendingService.GetItemsForUser();
-            double total = 0;
-            foreach (Spending spending in allSpendings)
-            {
-                if (spending.IsUseful)
-                {
-                    total += spending.Amount;
-                }
-            }
-            return total + "€";
-        }
+        get => _savedThisMonth;
+        set => this.RaiseAndSetIfChanged(ref _savedThisMonth, value);
     }
-    
-    public string TotalUseless 
+
+    private string _savedThisYear;
+    public string SavedThisYear
     {
-        get
-        {
-            List<Spending> allSpendings = _spendingService.GetItemsForUser();
-            double total = 0;
-            foreach (Spending spending in allSpendings)
-            {
-                if (!spending.IsUseful)
-                {
-                    total += spending.Amount;
-                }
-            }
-            return total + "€";
-        }
+        get => _savedThisYear;
+        set => this.RaiseAndSetIfChanged(ref _savedThisYear, value);
     }
-    
-    public double SavedMonth { get; set; }
-    public double SavedYear { get; set; }
-    
-    private double _reccurent = 0;
-    public double Reccurent
+
+    private string _reccurent;
+    public string Reccurent
     {
         get => _reccurent;
         private set => this.RaiseAndSetIfChanged(ref _reccurent, value);
     }
 
-    private async void SetReccurentAsync()
-    {
-        List<Spending> allSpendings = await _spendingService.GetAllSpendingsRecurentByMonth(true, DateTime.Now);
-        double total = 0;
-        foreach (Spending spending in allSpendings)
-        {
-            total += spending.Amount;
-        }
-        Reccurent = total;
-    }
-    
-    private List<string> _last3spendings = new() {"none", "none", "none"};
-    public List<string> Last3spendings
+    private List<Spending> _last3spendings;
+    public List<Spending> Last3spendings
     {
         get => _last3spendings;
-        private set => this.RaiseAndSetIfChanged(ref _last3spendings, value);
-    }
-
-    private async void SetLast3spendingsAsync()
-    {
-        List<Spending> allSpendings = await _spendingService.GetAllInMonth(DateTime.UtcNow);
-        List<string> stringList = new();
-        if (allSpendings.Count < 3) { throw new Exception("not enough elements in array"); }
-        int index = allSpendings.Count - 1;
-        stringList.Add(allSpendings[index].Title + " - " + allSpendings[index].Amount + "€");
-        stringList.Add(allSpendings[index -1].Title + " - " + allSpendings[index -1].Amount + "€");
-        stringList.Add(allSpendings[index -2].Title + " - " + allSpendings[index -2].Amount + "€");
-        Last3spendings = stringList;
+        set => this.RaiseAndSetIfChanged(ref _last3spendings, value);
     }
 
     private void OnDateChanged(DateTimeOffset date)
@@ -160,9 +99,35 @@ public class HomeDashboardViewModel:ViewModelBase, IDashboardListViewModel
 
     private async void SetValues(DateTimeOffset date)
     {
-        Spendings = await _spendingService.GetAllInMonth(date.DateTime);
-        Incomes = await _incomingService.GetAllInMonth(date.DateTime);
-        SavingProjects = await _savingProjectService.GetItemsForUser();
-        Savings = await _savingService.GetAllByUser();
+        var spendings = await _spendingService.GetAllInMonth(date.DateTime);
+        var incomes = await _incomingService.GetAllInMonth(date.DateTime);
+        var savingProjects = await _savingProjectService.GetItemsForUser();
+        var savings = await _savingService.GetAllByUser();
+
+        // Get the 3 last spendings
+        Last3spendings = spendings.OrderByDescending(s => s.Date).Take(3).ToList();
+
+        // get total of usefull and useless spendings
+        TotalUsefull = spendings.Where(s => s.IsUseful == true).Sum(s => s.Amount).ToString();
+        TotalUseless = spendings.Where(s => s.IsUseful == false).Sum(s => s.Amount).ToString();
+
+        // get total of recurrent spendings
+        Reccurent = spendings.Where(s => s.IsRecurring == true).Sum(s => s.Amount).ToString();
+
+        // get all savings from this month
+        DateTime firstDayOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        DateTime lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+        SavedThisMonth = savings.Where(
+            s => s.Date >= firstDayOfMonth && s.Date <= lastDayOfMonth
+        ).Sum(s => s.Amount).ToString();
+
+        DateTime currentDate = DateTime.Now;
+
+        // get all savings from this year
+        DateTime firstDayOfYear = new DateTime(currentDate.Year, 1, 1);
+        DateTime lastDayOfYear = new DateTime(currentDate.Year, 12, 31);
+
+        var savingsThisYear = savings.Where(s => s.Date >= firstDayOfYear && s.Date <= lastDayOfYear).ToList();
+        SavedThisYear = savingsThisYear.Sum(s => s.Amount).ToString();
     }
 }
